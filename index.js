f3aa7629a09e020780454de144b2798083b51c7e
Daniyal-thirtynorth <87475264+Daniyal-thirtynorth@users.noreply.github.com>
2021-12-01 19:18:16 +0500

    changed port to 5000

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.text())
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", req.headers.origin)
 	res.header("Access-Control-Allow-Credentials", true)
 	res.header("Access-Control-Allow-Methods", 'GET, DELETE, POST, PUT')
 	res.header("Access-Control-Allow-Headers", 'Authorization, Content-Type, Origin, X-Requested-With, X-Session-Id')
 	res.header("Access-Control-Expose-Headers", 'Location, X-Session-Id')
-	if(req.method === 'OPTIONS'){
+	if (req.method === 'OPTIONS') {
 		console.log('received options')
 		res.sendStatus(200)
 		return
 	}
-	else{
+	else {
 
 		next()
 	}
-})     
+})
 
 
 require('./src/auth')(app)
@@ -28,10 +28,10 @@ require('./src/following')(app)
 require('./src/profile')(app)
 require('./dbpost')(app)
 require('./articles')(app)
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const server = app.listen(port, () => {
-     const addr = server.address()
-     console.log(`Server listening at http://${addr.address}:${addr.port}`)
-     
+	const addr = server.address()
+	console.log(`Server listening at http://${addr.address}:${addr.port}`)
+
 })
 
