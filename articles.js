a4585df1c57db8f621602e8cb23b24707e4654e8
Daniyal-thirtynorth <87475264+Daniyal-thirtynorth@users.noreply.github.com>
2021-12-02 12:00:18 +0500

    added real db endpoints for add and get articles

--- articles.js
+++ articles.js
@@ -1,122 +1,161 @@
 const bodyParser = require('body-parser')
-let id=5;
-const addArticle = (req, res) => {
-     console.log('Payload received', req.body) 
-     var newarticle= {id:articles.length+1, author: req.body.author,date:req.body.date, text:req.body.text, comments:[]}
-     articles=[...articles,
-     newarticle]
-     res.send(newarticle)
+let articleModel = require('./model.js').Article
+let id = 5;
+const addArticle = async (req, res) => {
+    // console.log('Payload received', req.body)
+    // var newarticle = { id: articles.length + 1, author: req.body.author, date: req.body.date, text: req.body.text, comments: [] }
+    // articles = [...articles,
+    //     newarticle]
+    try {
+        const {
+            author,
+            text,
+        } = req.body
+
+        const allArticles = await articleModel.countDocuments({})
+        const newarticle = new articleModel({
+            id: allArticles + 1,
+            date: Date.now(),
+            author,
+            text,
+        })
+        await newarticle.save()
+        res.status(200).send(newarticle)
+    } catch (err) {
+        return res.status(400).json({ result: err.message })
+    }
 }
 
 
-const getArticle = (req, res) =>{
-    if(req.params.id){
-            console.log(articles)
-            let target = articles.filter((item)=>{return item.id == req.params.id })
-            console.log(articles.filter((item)=>{return item.id == req.params.id }))
-            console.log(target)
-            console.log(req.params.id)
-            if(target.length!==0){
-                res.send({articles:target});
-            }
-            else{
-                res.send({articles:[]})
-            }
+const getArticle = async (req, res) => {
+    // if (req.params.id) {
+    //     console.log(articles)
+    //     let target = articles.filter((item) => { return item.id == req.params.id })
+    //     console.log(articles.filter((item) => { return item.id == req.params.id }))
+    //     console.log(target)
+    //     console.log(req.params.id)
+    //     if (target.length !== 0) {
+    //         res.send({ articles: target });
+    //     }
+    //     else {
+    //         res.send({ articles: [] })
+    //     }
+    // }
+    // else {
+    //     res.send({ articles: articles });
+    // }
+    try {
+        const id = req.params.id
+        let filter = {}
+        if (id) {
+            filter = { id }
         }
-        else{
-            res.send({articles:articles});
-        }
-}
-const putArticle = (req,res)=>{
-    let p_id=req.params.id;
-    let newarticles=articles.filter((item)=>{return item.id!=p_id})
-    let selectedarticles=articles.filter((item)=>{return item.id==p_id})[0]
-    
-    if(req.body.commentId&&req.body.commentId>-1){
-        
-        let unselectedcomment=selectedarticles.comments.filter((item)=>{return item.commentId!=req.body.commentId})
-        res.send({articles:
-                [...newarticles,
-                    {selectedarticles,comments:
-                        [...unselectedarticles.comments,
-                            {commentId:req.body.commentId,text:req.body.text,author:'guest'}
-                            ]
-                        }
-                        ]
-                    }
-                    )
-    }
-    if(req.body.commentId&&req.body.commentId==-1){
-        
-        
-        res.send({articles:
-                [...newarticles,
-                    {selectedarticles,comments:
-                        [...unselectedarticles.comments,
-                            {commentId:1000,text:req.body.text,author:'guest'}
-                            ]
-                        }
-                        ]
-                    }
-                    )
-    }
-    else{
-        res.send({articles:
-            [...newarticles,{selectedarticles,text:req.body.text}]})
+        const articles = await articleModel.find(filter)
+        return res.status(200).json({ articles })
+    } catch (err) {
+        return res.status(400).json({
+            result: err.message
+        })
     }
 }
-const defaultmsg=(req,res)=>{
+const putArticle = async (req, res) => {
+    let p_id = req.params.id;
+
+    // let newarticles = articles.filter((item) => { return item.id != p_id })
+    // let selectedarticles = articles.filter((item) => { return item.id == p_id })[0]
+
+    // if (req.body.commentId && req.body.commentId > -1) {
+
+    //     let unselectedcomment = selectedarticles.comments.filter((item) => { return item.commentId != req.body.commentId })
+    //     res.send({
+    //         articles:
+    //             [...newarticles,
+    //             {
+    //                 selectedarticles, comments:
+    //                     [...unselectedarticles.comments,
+    //                     { commentId: req.body.commentId, text: req.body.text, author: 'guest' }
+    //                     ]
+    //             }
+    //             ]
+    //     }
+    //     )
+    // }
+    // if (req.body.commentId && req.body.commentId == -1) {
+
+
+    //     res.send({
+    //         articles:
+    //             [...newarticles,
+    //             {
+    //                 selectedarticles, comments:
+    //                     [...unselectedarticles.comments,
+    //                     { commentId: 1000, text: req.body.text, author: 'guest' }
+    //                     ]
+    //             }
+    //             ]
+    //     }
+    //     )
+    // }
+    // else {
+    //     res.send({
+    //         articles:
+    //             [...newarticles, { selectedarticles, text: req.body.text }]
+    //     })
+    // }
+}
+const defaultmsg = async (req, res) => {
     res.send("Hello pg39!")
 }
-module.exports= app =>{
-app.use(bodyParser.json())
-app.get('/',defaultmsg)
-app.post('/article', addArticle)
-app.get('/articles/:id?', getArticle)
-app.put('/articles/:id?', putArticle)}
+module.exports = app => {
+    app.use(bodyParser.json())
+    app.get('/test3', defaultmsg)
+    app.post('/article', addArticle)
+    app.get('/articles/:id?', getArticle)
+    app.put('/articles/:id?', putArticle)
+}
 
 // Get the port from the environment, i.e., Heroku sets it
 
 
 
-var articles =[{
-    id:1,
-    author:"pg39",
-    text:"This is my first article",
-    comments:[{author:'pg39test',commendId:1212,date:'2015-10-27T19:52:25.960Z',text:'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!'}],
-    date:'2017-04-04T15:09:04Z',
-    img:'http://az616578.vo.msecnd.net/files/2017/02/10/636223537065395516-1597768472_snow%204.jpg'
-    },
-    {
-    id:2,
-    author:"pg39",
-    text:"This is Karl's article",
-    comments:[{author:'pg39test2',commendId:1213,date:'2015-10-17T19:52:25.960Z',text:'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!'}],
-    date:'2011-04-04T15:07:04Z'
-    },
-    {
-    id:3,
-    author:"pg39",
-    text:"This is Robert's article",
-    comments:[{author:'pg39',commendId:1214,date:'2016-10-27T19:52:25.960Z',text:'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!'}],
-    date:'2016-04-04T15:07:04Z',
-    img:'https://s-media-cache-ak0.pinimg.com/originals/20/0b/95/200b95dfb2efa80d37479764a324b462.jpg'
-    },
-    {
-    id:4,
-    author:"pg39",
-    text:"Test article 4",
-    comments:[],
-    date:'2017-04-03T15:07:04Z',
-    img:'http://hpcsonline.org/wp-content/uploads/2016/01/Snow-row.jpg'
-    },
-    {
-    id:5,
-    author:"pg39",
-    text:"Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis diam. Pellentesque ut neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-    comments:[{author:'pg39test3',commendId:1215,date:'2015-11-27T19:52:25.960Z',text:'test comment'},
-    {author:'pg39',commendId:1216,date:'2015-11-28T19:52:25.960Z',text:'test comment more'}],
-    date:'2017-04-04T15:07:04Z',
-    img:'http://www.telegraph.co.uk/content/dam/Travel/ski/CourchevelTourisme-DomaineSkiable-36_header-large.jpg'
-    }
-    ]
+var articles = [{
+    id: 1,
+    author: "pg39",
+    text: "This is my first article",
+    comments: [{ author: 'pg39test', commendId: 1212, date: '2015-10-27T19:52:25.960Z', text: 'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!' }],
+    date: '2017-04-04T15:09:04Z',
+    img: 'http://az616578.vo.msecnd.net/files/2017/02/10/636223537065395516-1597768472_snow%204.jpg'
+},
+{
+    id: 2,
+    author: "pg39",
+    text: "This is Karl's article",
+    comments: [{ author: 'pg39test2', commendId: 1213, date: '2015-10-17T19:52:25.960Z', text: 'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!' }],
+    date: '2011-04-04T15:07:04Z'
+},
+{
+    id: 3,
+    author: "pg39",
+    text: "This is Robert's article",
+    comments: [{ author: 'pg39', commendId: 1214, date: '2016-10-27T19:52:25.960Z', text: 'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!' }],
+    date: '2016-04-04T15:07:04Z',
+    img: 'https://s-media-cache-ak0.pinimg.com/originals/20/0b/95/200b95dfb2efa80d37479764a324b462.jpg'
+},
+{
+    id: 4,
+    author: "pg39",
+    text: "Test article 4",
+    comments: [],
+    date: '2017-04-03T15:07:04Z',
+    img: 'http://hpcsonline.org/wp-content/uploads/2016/01/Snow-row.jpg'
+},
+{
+    id: 5,
+    author: "pg39",
+    text: "Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis diam. Pellentesque ut neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+    comments: [{ author: 'pg39test3', commendId: 1215, date: '2015-11-27T19:52:25.960Z', text: 'test comment' },
+    { author: 'pg39', commendId: 1216, date: '2015-11-28T19:52:25.960Z', text: 'test comment more' }],
+    date: '2017-04-04T15:07:04Z',
+    img: 'http://www.telegraph.co.uk/content/dam/Travel/ski/CourchevelTourisme-DomaineSkiable-36_header-large.jpg'
+}
+]
--- src/uploadCloudinary.js
+++ src/uploadCloudinary.js
@@ -2,32 +2,57 @@
 const multer = require('multer')
 const stream = require('stream')
 const cloudinary = require('cloudinary')
-
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 if (!process.env.CLOUDINARY_URL) {
-     process.env.CLOUDINARY_URL="cloudinary://863665439564258:nDl08K4Dz-nVl13fkk3Qrz_syXc@hmgzratvw"
+	process.env.CLOUDINARY_URL = "cloudinary://863665439564258:nDl08K4Dz-nVl13fkk3Qrz_syXc@hmgzratvw"
 }
-
+cloudinary.config({
+	cloud_name: 'dz4ukzs1e',
+	api_key: '579985335747488',
+	api_secret: 'q94eiE2wEN-zKpclh3E3LE_4Vyc'
+});
 const doUpload = (publicName, req, res, next) => {
 
-	const uploadStream = cloudinary.uploader.upload_stream(result => {    	
-         req.fileurl = result.url
-         req.fileid = result.public_id
-		 console.log(result.url)
-         next()
-	}, { public_id: req.body[publicName]})
-	console.log(publicName)
-	console.log(req.headers)
+	// const uploadStream = cloudinary.uploader.upload_stream(result => {
+	// 	req.fileurl = result.url
+	// 	req.fileid = result.public_id
+	// 	console.log(result.url)
+	// 	next()
+	// }, { public_id: req.body[publicName] })
 
-	const s = new stream.PassThrough()
-	s.end(req.file.buffer)
-	s.pipe(uploadStream)
-	s.on('end', uploadStream.end)
+
+	// const uploadStream = cloudinary.uploader.upload_stream(result => {
+	// 	console.log("📝📝result was📝📝", result)
+	// 	req.fileurl = result.url
+	// 	req.fileid = result.public_id
+	// 	console.log(result.url)
+	// 	next()
+	// }, { public_id: "olympic_flag" })
+	console.log("📝📝req was📝📝", req.file)
+	cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+		{ public_id: "olympic_flag" },
+		function (error, result) {
+			req.fileurl = result.url
+			req.fileid = result.public_id
+			console.log(result.url)
+			console.log(result);
+			next()
+		}
+	);
+	// console.log(publicName)
+	// console.log(req.headers)
+	// const s = new stream.PassThrough()
+	// console.log("🎈🎈🎈🎈🎈🎈🎈🎈", req.file)
+	// s.end(req.file.buffer)
+	// s.pipe(uploadStream)
+	// s.on('end', uploadStream.end)
 	console.log('upload over')
 }
 
 const uploadImage = (publicName) => (req, res, next) =>
-     multer().single('image')(req, res, () => 
-               doUpload(publicName, req, res, next))
+	multer().single('image')(req, res, () =>
+		doUpload(publicName, req, res, next))
 
 
 module.exports = uploadImage
